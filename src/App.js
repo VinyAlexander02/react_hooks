0859e01c2cb289f193e3320d83924b9f7e67bb1c
Vinicius Alexander Moreira Nascimento <vinicius.malex2002@gmail.com>
2024-10-16 17:51:59 -0300

    Work with useMemo

--- src/App.js
+++ src/App.js
@@ -1,70 +1,127 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo } from 'react';
+import P from 'prop-types';
 
-const eventFn = () => {
-  console.log('H1 clicado');
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
+
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  // const { reverse } = this.state;
-  const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [counter2, setCounter2] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  const handleClick = () => {
-    setReverse(!reverse);
-  };
+  console.log('Pai renderizou!');
 
-  const handleIncrement = () => {
-    setCounter(counter + 1);
-  };
-
-  const handleIncrement2 = () => {
-    setCounter2(counter2 + 1);
-  };
-
-  // ComponentDidUpdate - executa todas as vezes q o componente atualiza
-  // useEffect(() => {
-  //   console.log('ComponentDidUpdate');
-  // });
-
-  // ComponentDidMount - executa 1x
+  // Component did mount
   useEffect(() => {
-    document.querySelector('h1')?.addEventListener('click', eventFn);
-
-    // ComponentWillUmount - limpeza
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventFn);
-    };
+    setTimeout(function () {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((r) => r.json())
+        .then((r) => setPosts(r));
+    }, 5000);
   }, []);
-
-  // Com dependência - executa todas as vezez que a dependência mudar
-  useEffect(() => {
-    console.log(`C1 ${counter} C2; ${counter2}`);
-  }, [counter, counter2]);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <h1>
-          Counter: {counter}, Counter2: {counter2}
-        </h1>
-        <button type="button" onClick={handleClick}>
-          reverse {reverseClass}
-        </button>
-        <button type="button" onClick={handleIncrement}>
-          Counter
-        </button>
-        <button type="button" onClick={handleIncrement2}>
-          Counter 2
-        </button>
-      </header>
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </p>
+      {useMemo(() => {
+        return (
+          posts.length > 0 &&
+          posts.map((post) => {
+            return <Post key={post.id} post={post} />;
+          })
+        );
+      }, [posts])}
+      {posts.length <= 0 && <p>Ainda não existem posts.</p>}
     </div>
   );
 }
+
+export default App;
+
+// const eventFn = () => {
+//   console.log('H1 clicado');
+// };
+
+// function App() {
+//   // const { reverse } = this.state;
+//   const [reverse, setReverse] = useState(false);
+//   const [counter, setCounter] = useState(0);
+//   const [counter2, setCounter2] = useState(0);
+//   const reverseClass = reverse ? 'reverse' : '';
+
+//   const handleClick = () => {
+//     setReverse(!reverse);
+//   };
+
+//   const handleIncrement = () => {
+//     setCounter(counter + 1);
+//   };
+
+//   const handleIncrement2 = () => {
+//     setCounter2(counter2 + 1);
+//   };
+
+//   // ComponentDidUpdate - executa todas as vezes q o componente atualiza
+//   // useEffect(() => {
+//   //   console.log('ComponentDidUpdate');
+//   // });
+
+//   // ComponentDidMount - executa 1x
+//   useEffect(() => {
+//     document.querySelector('h1')?.addEventListener('click', eventFn);
+
+//     // ComponentWillUmount - limpeza
+//     return () => {
+//       document.querySelector('h1')?.removeEventListener('click', eventFn);
+//     };
+//   }, []);
+
+//   // Com dependência - executa todas as vezez que a dependência mudar
+//   useEffect(() => {
+//     console.log(`C1 ${counter} C2; ${counter2}`);
+//   }, [counter, counter2]);
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
+//         <h1>
+//           Counter: {counter}, Counter2: {counter2}
+//         </h1>
+//         <button type="button" onClick={handleClick}>
+//           reverse {reverseClass}
+//         </button>
+//         <button type="button" onClick={handleIncrement}>
+//           Counter
+//         </button>
+//         <button type="button" onClick={handleIncrement2}>
+//           Counter 2
+//         </button>
+//       </header>
+//     </div>
+//   );
+// }
 
 // class App extends Component {
 //   state = {
@@ -92,5 +149,3 @@ function App() {
 //     );
 //   }
 // }
-
-export default App;
